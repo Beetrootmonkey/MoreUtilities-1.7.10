33f84d7aae4e5a9390c69517f0847982af8e804e
I Am The Issue <n.adelhard@googlemail.com>
2017-01-21 03:11:06 +0100

    Initital Setup
    The mod is now ready for further developement

---
+++ src/main/java/iamtheissue/moreutilities/config/Config.java
@@ -0,0 +1,39 @@
+package iamtheissue.moreutilities.config;
+
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.common.config.Configuration;
+
+public class Config
+{
+	private static String CATEGORY_DROPS = "drops";
+	private static String CATEGORY_RECIPES = "recipes";
+	public static void load(FMLPreInitializationEvent event)
+	{
+		Configuration config = new Configuration(event.getSuggestedConfigurationFile());
+		config.load();
+		
+		//boolean someConfigFlag = config.get(Configuration.CATEGORY_GENERAL, "SomeConfigFlag", false).getBoolean(false);
+		//Notice there is nothing that gets the value of this property so the expression results in a Property object.
+        //Property someProperty = config.get(Configuration.CATEGORY_GENERAL, "SomeConfigString", "nothing");
+        
+        // Here we add a comment to our new property.
+        //someProperty.comment = "This value can be read as a string!";
+        
+        //String someConfigString = someProperty.getString();
+				
+        config.setCategoryComment(CATEGORY_DROPS, "If false, disables the alteration of this mob's drops.");
+        Features.horseDrops = config.get(CATEGORY_DROPS, "Horse", true).getBoolean(true);
+        Features.sheepDrops = config.get(CATEGORY_DROPS, "Sheep", true).getBoolean(true);
+        Features.spiderDrops = config.get(CATEGORY_DROPS, "Spider", true).getBoolean(true);
+        Features.squidDrops = config.get(CATEGORY_DROPS, "Squid", true).getBoolean(true);
+        
+        config.setCategoryComment(CATEGORY_RECIPES, "If false, disables this recipe.");
+        Features.reedsToSticks = config.get(CATEGORY_RECIPES, "ReedsToSticks", true).getBoolean(true);
+        Features.coalPellet = config.get(CATEGORY_RECIPES, "CoalPellet", true).getBoolean(true);
+        Features.charcoalPellet = config.get(CATEGORY_RECIPES, "CharcoalPellet", true).getBoolean(true);
+        
+
+        config.save();
+	}
+	
+}
---
+++ src/main/java/iamtheissue/moreutilities/config/Features.java
@@ -0,0 +1,13 @@
+package iamtheissue.moreutilities.config;
+
+public class Features
+{
+	public static boolean sheepDrops = true;
+	public static boolean squidDrops = true;
+	public static boolean spiderDrops = true;
+	public static boolean horseDrops = true;
+	public static boolean reedsToSticks = true;
+	public static boolean coalPellet = true;
+	public static boolean charcoalPellet = true;
+	
+}
